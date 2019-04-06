db203760a59d4a71f7b8318d896ebaff4ee498d4
Ricky Gilleland <ricky.gilleland@gmail.com>
2019-04-06 18:21:05 -0400

    Update config

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+$database_url = getenv('JAWSDB_URL');
+$dbparts = parse_url($database_url);
+
 return [
 
     /*
@@ -44,11 +47,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $dbparts['host']),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', ltrim($dbparts['path'],'/')),
+            'username' => env('DB_USERNAME', $dbparts['user']),
+            'password' => env('DB_PASSWORD', $dbparts['pass']),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
