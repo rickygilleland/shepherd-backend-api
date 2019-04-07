49b5b0d1be10b5602e4a1db34afd7abcf6f5e221
Ricky Gilleland <ricky.gilleland@gmail.com>
2019-04-07 17:22:02 -0400

    Remove test code and fix posts query

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,20 +12,10 @@ class PostsController extends Controller
     
     public function get_posts(Request $request)
 	{
-		
-		//create a dummy post every time this request is made for testing
-		$post = new \App\Post();	
-		$post->content = Str::random(60);
-		$post->user_id = Auth::id();
-		$post->location_lat	= "89";
-		$post->location_long = "90";
-		
-		$post->save();
-		
-		
+
 		$twenty_four_hours_ago = strtotime("24 hours ago");
 		
-		$posts = \App\Post::where('created_at', '<', $twenty_four_hours_ago)->get();
+		$posts = \App\Post::where('created_at', '>', $twenty_four_hours_ago)->get();
 		
 		foreach ($posts as &$post) {
 			unset($post->location_lat);
