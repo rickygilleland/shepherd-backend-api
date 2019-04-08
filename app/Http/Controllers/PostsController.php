cd3608e60a642420cf57e8ce28b060f5e16e86ca
Ricky Gilleland <ricky.gilleland@gmail.com>
2019-04-07 20:16:57 -0400

    Get recent posts by location

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,38 @@ class PostsController extends Controller
 
 		$twenty_four_hours_ago = strtotime("24 hours ago");
 		
-		$posts = \App\Post::where('created_at', '>', $twenty_four_hours_ago)->get();
+		SELECT
+			  id, (
+			    6371 * acos (
+			      cos ( radians(37.785834) )
+			      * cos( radians( location_lat ) )
+			      * cos( radians( location_long ) - radians(-122.406417) )
+			      + sin ( radians(37.785834) )
+			      * sin( radians( location_lat ) )
+			    )
+			  ) AS distance
+			FROM posts
+			HAVING distance < 30
+			ORDER BY distance
+			LIMIT 0 , 20;
+			
+			
+			$orders = DB::table('orders')
+                ->selectRaw('price * ? as price_with_tax', [1.0825])
+                ->get();
+		$posts = DB::table('posts')->selectRaw("
+			  id, (
+			    6371 * acos (
+			      cos ( radians(".$request->location_lat.") )
+			      * cos( radians( location_lat ) )
+			      * cos( radians( location_long ) - radians(".$request->location_long.") )
+			      + sin ( radians(".$request->location_lat.") )
+			      * sin( radians( location_lat ) )
+			    )
+			  ) AS distance,
+			  content,
+			")->havingRaw("HAVING distance < 2")->get();
+
 		
 		foreach ($posts as &$post) {
 			unset($post->location_lat);
