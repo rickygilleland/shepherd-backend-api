deca0acb15f29166c51ee3ee2fe0e764714705f6
Ricky Gilleland <ricky.gilleland@gmail.com>
2019-04-25 16:53:34 -0400

    Add auth0 authentication code and debugging output

--- app/Http/Middleware/CheckAuth0JWT.php
+++ app/Http/Middleware/CheckAuth0JWT.php
@@ -4,6 +4,12 @@
 namespace App\Http\Middleware;
 
 use Auth0\SDK\JWTVerifier;
+use Auth0\Login\Auth0User;
+use Auth0\Login\Auth0JWTUser;
+use Auth0\Login\Repository\Auth0UserRepository;
+
+use Auth0\SDK\API\Authentication;
+
 use Closure;
 
 class CheckAuth0JWT {
@@ -24,6 +30,28 @@ class CheckAuth0JWT {
         try {
             $jwtVerifier = new JWTVerifier($jwtConfig);
             $decodedToken = $jwtVerifier->verifyAndDecode($accessToken);
+            
+            $auth0_api = new Authentication(
+			    $laravelConfig['domain'],
+			    $laravelConfig['client_id'],
+			    $laravelConfig['client_secret'],
+			);
+			
+			$user = $auth0_api->userinfo($accessToken);
+            
+            print_r($user); die();
+            
+            if (!$user) {
+	            return \Response::make('Unauthorized', 401);
+	        }
+	        if (time() > $user->exp) {
+	            return new JsonResponse([
+	                'message'   =>  'Token expired'
+	            ], 401);
+	        }
+
+	        // lets log the user in so it is accessible
+	        \Auth::login($user);
             
             print_r($decodedToken); die();
         } catch (\Exception $e) {
